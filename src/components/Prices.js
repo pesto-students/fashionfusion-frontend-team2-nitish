5f6f58de425231d9c0609fbcff6d237d3bad23b5
jk872001 <jiteshkumar872001@gmail.com>
2023-03-13 16:51:40 +0530

    filter applied

--- src/components/Prices.js
+++ src/components/Prices.js
@@ -1,32 +1,32 @@
 export const Prices = [
   {
     _id: 0,
-    name: "$0 to 19",
-    array: [0, 19],
+    name: "$0 to 100",
+    array: [0, 100],
   },
   {
     _id: 1,
-    name: "$20 to 39",
-    array: [20, 39],
+    name: "$100 to 200",
+    array: [100,200],
   },
   {
     _id: 2,
-    name: "$40 to 59",
-    array: [40, 59],
+    name: "$200 to 300",
+    array: [200,300],
   },
   {
     _id: 3,
-    name: "$60 to 79",
-    array: [60, 79],
+    name: "$300 to 400",
+    array: [300,400],
   },
   {
     _id: 4,
-    name: "$80 to 99",
-    array: [80, 99],
+    name: "$400 to 500",
+    array: [400,500],
   },
   {
     _id: 4,
-    name: "$100 or more",
-    array: [100, 9999],
+    name: "$500 or more",
+    array: [500, 9999],
   },
 ];
