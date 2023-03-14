825a817d1bdaf5cc3697434e50f2ccf8e43f9d25
Jitesh-Kumar <72179040+jk872001@users.noreply.github.com>
2023-03-14 18:07:47 +0530

    Update Orders.js

--- src/pages/user/Orders.js
+++ src/pages/user/Orders.js
@@ -17,9 +17,9 @@ const Orders = () => {
     }
   };
 
-  useEffect(() => {
-    if (auth?.token) getOrders();
-  }, [auth?.token]);
+//   useEffect(() => {
+//     if (auth?.token) getOrders();
+//   }, [auth?.token]);
   return (
     <Layout title={"Your Orders"}>
       <div className="container-flui p-3 m-3 dashboard">
