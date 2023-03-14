cb869e0823026552829dca59557522b5b8071bbc
Jitesh-Kumar <72179040+jk872001@users.noreply.github.com>
2023-03-14 12:45:40 +0530

    Update Footer.js

--- src/components/Layout/Footer.js
+++ src/components/Layout/Footer.js
@@ -14,7 +14,7 @@ const Footer = () => {
   <div className="row px-xl-5 pt-5">
     <div className="col-lg-4 col-md-12 mb-5 pr-3 pr-xl-5">
       <a href className="text-decoration-none">
-        <h1 className="mb-4 display-5 font-weight-semi-bold"><span className="text-primary font-weight-bold border border-white px-3 mr-1">Fashion</span>Fusion</h1>
+        <h1 className="mb-4 display-5 font-weight-semi-bold"><span className="text-primary font-weight-bold border border-white px-3 mr-1">Fashion</span></h1>
       </a>
       <p>Dolore erat dolor sit lorem vero amet. Sed sit lorem magna, ipsum no sit erat lorem et magna ipsum dolore amet erat.</p>
       <p className="mb-2"><i className="fa fa-map-marker-alt text-primary mr-3" />123 Street, New York, USA</p>
