a33cabb7a92a7451387ec6954cc15820912e9ffe
Jitesh-Kumar <72179040+jk872001@users.noreply.github.com>
2023-03-17 17:22:28 +0530

    Update CategoryForm.js

--- src/components/Form/CategoryForm.js
+++ src/components/Form/CategoryForm.js
@@ -3,6 +3,8 @@ import React from "react";
 const CategoryForm = ({ handleSubmit, value, setValue }) => {
   return (
     <>
+    
+    
       <form onSubmit={handleSubmit}>
         <div className="mb-3">
           <input
